7839c0a52a3e2807735788ef7de6e60cd0ba3c54
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-06 19:21:48 -0400

    Navbar OK

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,19 +3,18 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from '../components/auth/Login/Login.jsx'
 import Register from '../components/auth/Register/Register.jsx'
 import Home from '../components/Home/Home.jsx'
+import { Navbar } from '../components/Navbar/Navbar.jsx'
 
 const AppRouter = () => {
   return (
     <>
-    
+    <Navbar/>
     <Routes>
-
         <Route path="login" element={<Login/>} />
         <Route path="register" element={<Register/>}/>
         <Route path="home" element={<Home/>}/>
 
         <Route path="/" element={<Navigate to="/login"/>}/>
-
     </Routes>
     
     </>
