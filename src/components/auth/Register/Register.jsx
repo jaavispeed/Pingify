434b637444ce733e500b78690e68cd137a06c5f8
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-04-09 17:39:12 -0400

    Validaciones Register

--- src/components/auth/Register/Register.jsx
+++ src/components/auth/Register/Register.jsx
@@ -1,29 +1,51 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import { ToastContainer } from 'react-toastify';
 
 const Register = () => {
+  const navigate = useNavigate()
+
   const { register } = useContext(AuthContext);  // Consumir la función de register
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleRegister = async (e) => {
+  const [isValid, setIsValid] = useState(false);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+
+
+  useEffect(() => {
+    const isEmailValid = emailRegex.test(email);
+    const isPasswordValid = password.length >= 6
+    const isUsername = username.length >= 2;
+    setIsValid(isEmailValid && isPasswordValid && isUsername);
+  }, [email, password]);
+
+  const onRegister = async (e) => {
     e.preventDefault();
     try {
-      await register(username, email, password);  // Usar la función register
+      await register(username, email, password);
+      navigate('/login', { replace: true })
     } catch (error) {
       console.error('Error al registrar:', error);
+      toast.error(error.message || 'Error al registrarse');
+
     }
   };
 
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="absolute top-4 left-1/2 transform -translate-x-1/2 font-bold text-4xl text-center">
+        Pingify
+      </div>
       <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-sm">
         <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">Crear Cuenta</h2>
 
-        <form onSubmit={handleRegister}>
+        <form onSubmit={onRegister}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm mb-2" htmlFor="name">
               Usuario
@@ -32,11 +54,14 @@ const Register = () => {
               id="name"
               type="text"
               value={username}
-              onChange={(e) => setUsername(e.target.value)} 
+              onChange={handleUsernameChange}
               placeholder="Nombre de usuario"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
+            {username && username.length < 2 && (
+              <p className="text-red-500 text-sm mt-1">El nombre de usuario debe tener un mínimo de 2 caracteres.</p>
+            )}
           </div>
 
           <div className="mb-4">
@@ -47,11 +72,14 @@ const Register = () => {
               id="email"
               type="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleEmailChange}
               placeholder="tucorreo@ejemplo.com"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
+            {!emailRegex.test(email) && email && (
+              <p className="text-red-500 text-sm mt-1">Formato de email no válido</p>
+            )}
           </div>
 
           <div className="mb-6">
@@ -62,23 +90,28 @@ const Register = () => {
               id="password"
               type="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
               placeholder="Crea una contraseña"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
+            {password && password.length < 6 && (
+              <p className="text-red-500 text-sm mt-1">La contraseña debe tener al menos 6 caracteres</p>
+            )}
           </div>
 
           <button
             type="submit"
-            className="w-full bg-black hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
+            className={`w-full ${isValid ? 'bg-black hover:bg-gray-800 cursor-pointer' : 'bg-gray-400 cursor-not-allowed'} text-white font-semibold py-2 px-4 rounded-lg transition duration-200`}
+            disabled={!isValid}
+
           >
             Registrarse
           </button>
           <p className='font-semibold text-sm'>¿Ya tienes cuenta? <Link to='/login' className='text-blue-700 font-bold'>Ingresa aquí</Link></p>
-
         </form>
       </div>
+      <ToastContainer />
     </div>
   );
 };
